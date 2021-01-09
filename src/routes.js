82bddbf825477875541d05e9dd9d34b30fc0d580
brenogcota <brenocota4@gmail.com>
2021-01-09 20:24:05 -0300

    fix

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Main} />
+                <Route exact path="/" component={Main} />
             </Switch>
         </BrowserRouter>
     );

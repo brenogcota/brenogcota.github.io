09b5897c6996d514118bcf9d5510a54e80d4f0f9
brenogcota <brenocota4@gmail.com>
2021-01-09 22:03:36 -0300

    Updates

--- precache-manifest.5a2f48ab8ba5d763a0bd73bc2036470d.js
+++ precache-manifest.5a2f48ab8ba5d763a0bd73bc2036470d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03539196e0a3f1e5f87d65e745a2ec1e",
+    "revision": "7ad563d468228e695d41b8fe8868468f",
     "url": "/index.html"
   },
   {
-    "revision": "10108d06464e1fd5a447",
-    "url": "/static/js/2.51a9a337.chunk.js"
+    "revision": "bc4272a09c5ddd66371b",
+    "url": "/static/js/2.92088f64.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.51a9a337.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.92088f64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d33217a350453b23879",
-    "url": "/static/js/main.e40f4ce2.chunk.js"
+    "revision": "0a79bd7174c6c8667b6b",
+    "url": "/static/js/main.8c3f2aef.chunk.js"
   },
   {
     "revision": "3f809adccd7b5eb81ce7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fb6668332af6c59c1ef1d167ac4f620.js"
+  "/precache-manifest.5a2f48ab8ba5d763a0bd73bc2036470d.js"
 );
 
 self.addEventListener('message', (event) => {
